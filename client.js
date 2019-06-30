e8fc658ad67de0735146bef1c8d0bf4348eeb3c2
Ubuntu <ubuntu@ip-172-31-23-194.us-east-2.compute.internal>
2019-06-30 14:46:30 +0000

    sa

--- client.js
+++ client.js
@@ -6,7 +6,14 @@ const axios = require('axios');
 
 app.get('/:id', (req, res) => {
 socket.emit('FINISH', req.params.id);
+let lol;
+socket.on('TEST', (data) => { console.log(data);
+return res.send(data)});
 });
+
+app.get('/get/:id', (req, res) => {
+client.emit('GETTOP', req.params.id);
+console.log(req.params.id)});
 app.listen(PORT, () => {console.log('server client on :' + PORT)});
 
 
--- socketManager.js
+++ socketManager.js
@@ -14,12 +14,19 @@ const dbknex = {
 
 const knex = require('knex')(dbknex);
 
-module.exports = function(socket) {
+module.exports = async function(socket) {
     var client = new zerorpc.Client();
 //console.log(client);
     client.connect("tcp://127.0.0.1:4242");
     console.log('socket id: ' + socket.id);
-client.invoke('streaming_range', 1,2,3);
+await client.invoke('streaming_range', 1,2,3, (req, res) =>{console.log(res)});
+
+socket.on('GETTOP', async(id) => {
+console.log(id, 'hi from gettop'); 
+await client.invoke('streaming_range', 1,2,3, (req, res) => {
+io.emit('RETURNDATA', res);
+});
+});
 
 	socket.on('FINISH', async (exp) =>{
     await knex.from('instances').select('*').where('experiment', '=', exp)
@@ -27,5 +34,10 @@ client.invoke('streaming_range', 1,2,3);
 let newarr = [];
 await arr.forEach((item) => {newarr.push(JSON.parse(item.jsonFile))});
 console.log(newarr);
- client.invoke('streaming_range', 1, newarr, 3)})})
+ client.invoke('streaming_range', 1, newarr, 3, (error,res, more) => {
+console.log(res, error, more);
+socket.emit('TEST', res);
+})
+})
+})
 };
