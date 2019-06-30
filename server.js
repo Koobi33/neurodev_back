017d12d2608d15321a60422c429223c621b1d6df
Suchlov Dmitry <45110271+Koobi33@users.noreply.github.com>
2019-06-30 06:03:06 +0300

    's'

--- server.js
+++ server.js
@@ -39,19 +39,21 @@ app.get('/', (req, res) => {
     )});
 
 app.post('/input', async (req, res) => {
-   //save data to db
-await knex('instances').insert({jsonFile: req.body.data, experiment: req.body.tag}).then(() =>{console.log('ok');
+    //save data to db
+    await knex('instances').insert({jsonFile: req.body.data, experiment: req.body.tag}).then(() => {
+        console.log('ok');
+    })
 });
+    app.post('/finish', (req, res) => {
+        //get BIG data from db and send to zerorpcserver
+        //waiting rpc answer and send res to client!
 
-app.post('/finish', (req, res) => {
-    //get BIG data from db and send to zerorpcserver
-    //waiting rpc answer and send res to client!
+    });
 
-});
+    io.on('connection', SocketManager);
 
-io.on('connection', SocketManager);
+    http.listen(PORT, () => {
+        console.log('server started on ' + PORT);
+    });
 
-http.listen(PORT, () => {
-    console.log('server started on ' + PORT);
-});
 
